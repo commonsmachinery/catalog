e1800fc191bb1f757f8bf00e1c2f2acd5c806ed3
Lexas <dislexas@gmail.com>
2014-02-15 01:37:42 -0700

    adding jade

--- frontend/server.js
+++ frontend/server.js
@@ -1,23 +1,26 @@
-/* Catalog web/REST frontend - main script
+/* 
+  Catalog web/REST frontend - main script
 
-   Copyright 2014 Commons Machinery http://commonsmachinery.se/
+  Copyright 2014 Commons Machinery http://commonsmachinery.se/
 
-   Authors: Peter Liljenberg <peter@commonsmachinery.se>
+  Authors: Peter Liljenberg <peter@commonsmachinery.se>
 
-   Distributed under an AGPL_v3 license, please see LICENSE in the top dir.
-  */
+  Distributed under an AGPL_v3 license, please see LICENSE in the top dir.
+*/
 
 'use strict';
 
-var debug = require('debug')('frontend:server');
-var express = require('express');
+/* ======================== Load Node Modules ======================== */
 //var redis = require('redis');
 var celery = require('node-celery');
-
-var store = require('./lib/store');
+var cons = require('consolidate'); 
+var debug = require('debug')('frontend:server');
+var express = require('express');
 
 //var redisClient = redis.createClient();
 
+
+/* ============================== Backend ============================== */
 var backend = celery.createClient({
     CELERY_BROKER_URL: 'amqp://guest@localhost:5672//',
     CELERY_RESULT_BACKEND: 'amqp',
@@ -29,27 +32,30 @@ backend.on('error', function(err) {
     console.error('celery error: %s', err);
 });
 
-
-//
-// Main frontend setup
-//
-
-var app = express();
-
-// Middlewares
-app.use(express.logger());
-app.use(express.json());
-
-
-// Add in our endpoint logic
-require('./lib/rest.js')(app, backend, 'http://localhost:8004');
-
-
 // Kick everything off 
-
 backend.on('connect', function() {
     console.log('celery is ready, starting web server');
 
     app.listen(8004);
     console.log('listening on port 8004');
 });
+
+
+/* ============================== Frontend Setup ========================= */
+var app = express();
+
+// Middlewares
+app.use(express.logger());
+app.use(express.json());
+
+// Templating
+app.engine('.jade', cons.jade);
+app.set('view engine', 'jade');
+
+// Add in our endpoint logic
+require('./lib/rest.js')(app, backend, 'http://localhost:8004');
+
+
+/* ============================= Requests =============================== */
+
+
