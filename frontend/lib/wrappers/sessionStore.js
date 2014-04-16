d6953c6bdbd5a477640b876730464a381b91f6bd
Peter Liljenberg <peter.liljenberg@gmail.com>
2014-04-16 20:48:01 +0200

    [frontend] Parse MongoDB URL for sessionstore with url module and set all parameters

--- frontend/lib/wrappers/sessionStore.js
+++ frontend/lib/wrappers/sessionStore.js
@@ -3,98 +3,38 @@
 var debug = require('debug')('frontend:sessionStore');
 var sessionStore = require('sessionstore');
 var Promise = require('bluebird');
+var url = require('url');
 
 var promiseFilter;
 
-module.exports = function(host, dbname, port){
+module.exports = function() {
     function promise (resolve, reject) {
-        function settle (err, store){
-            if(err){
+        var u = url.parse(process.env.CATALOG_MONGODB_URL);
+        var username, password, f;
+
+        if (u.auth) {
+            f = u.auth.split(':', 2);
+            username = f[0];
+            password = f.length > 1 ? f[1] : null;
+        }
+
+        sessionStore.createSessionStore({
+            type: 'mongodb',
+            dbName: process.env.CATALOG_USERS_DB,
+            host: u.hostname || 'localhost',
+            port: u.port || 27017,
+            username: username,
+            password: password,
+        }, function (err, store) {
+            if (err) {
                 reject(err);
                 return;
             }
+
             debug('session store connected');
-
-            extend(store, store.constructor.prototype);
-
             resolve(store);
-            return;
-        }
-
-        host = host.match(/\/\/([^:\/?#]*)/)[1];
-        console.log(host, port, dbname);
-        sessionStore.createSessionStore({
-            type: 'mongodb',
-            dbName: dbname,
-            host: host,
-            port: port
-        }, settle);
-
-        return;
+        });
     }
+
     return new Promise(promise);
 };
-
-function extend (store, proto) {
-
-    store.list = {};
-
-    /* lets us get filtered sessions */
-    proto.all = promiseFilter;
-
-    /* keeps a registry of currently connected users */
-    proto.set = (function(_super){
-        return function(sid, sess, callback){
-            var sessRef = this.list[sid];
-            if(!sessRef){
-                sessRef = sess;
-            }
-            _super.apply(this, arguments);
-            return;
-        };
-    })(proto.set);
-
-    /* kills other user's sessions */
-    proto.kick = function(sid){
-        var sessRef = this.list[sid];
-        if(sessRef){
-            sessRef.destroy();
-        }
-        else{
-            store.destroy(sid);
-        }
-        return;
-    };
-}
-
-function promiseFilter(param, offset, limit) {
-
-    var self = this;
-    function promise (resolve, reject) {
-        
-        var arr = [];
-        function pushSession(err, d) {
-            if(d && d.uid){
-                arr.push(d);
-            }
-            else{
-               resolve(arr);
-            }
-            return;
-        }
-
-        self.sessions.find(param, function(err, cursor){ 
-            if(err){
-                console.error('error getting sessions: ', err);
-                reject(err);
-            }
-            cursor.skip(offset || 0);
-            cursor.limit(limit || 0);
-            cursor.each(pushSession);
-            return;
-        });
-
-        return;
-    }
-    return new Promise(promise);
-}
