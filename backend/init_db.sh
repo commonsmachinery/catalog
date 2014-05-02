bf6f38efbe170c5e178a85934e6145c529cded70
Peter Liljenberg <peter.liljenberg@gmail.com>
2014-05-02 19:09:41 +0200

    [backend] Move DB store init code into separate python script

--- backend/init_db.sh
+++ backend/init_db.sh
@@ -1,27 +1,5 @@
 #! /bin/bash
 
-function init_stores() {
-python <<END
-import RDF, os
-
-from catalog.config import config
-from catalog.store import MainStore
-
-for name in ['public', 'works']:
-    storage_type, options = MainStore.get_store_options(name)
-
-    # workaround: sqlite doesn't support 'dir' so prepend directory to the name
-    if storage_type == 'sqlite':
-        name = os.path.abspath(os.path.join(config.CATALOG_DATA_DIR, name))
-
-    options = options + "new='true'"
-
-    store = RDF.Storage(storage_name=storage_type, name=name, options_string=options)
-    model = RDF.Model(store)
-    model.sync()
-END
-}
-
 if [ "$init_db_mode" != "docker" ]
 then
     # running locally, init some variables
@@ -31,16 +9,7 @@ then
 
     mkdir -p "$CATALOG_DATA_DIR"
 
-    if [ -n "$VIRTUAL_ENV" ]
-    then
-        init_stores
-        cd $BACKEND_BASE_DIR
-    else
-        . $BACKEND_BUILD_DIR/bin/activate
-        init_stores
-        cd $BACKEND_BASE_DIR
-        deactivate
-    fi
+    $BACKEND_BUILD_DIR/bin/python "$(dirname "$0")/init_db.py"
 else
     # running in docker, need different variables
     export CATALOG_BACKEND_STORE_TYPE="postgresql"
@@ -50,5 +19,5 @@ else
     export CATALOG_BACKEND_STORE_DB_USER="docker"
     export CATALOG_BACKEND_STORE_DB_PASSWORD="docker"
 
-    init_stores
+    "$(dirname "$0")/init_db.py"
 fi
